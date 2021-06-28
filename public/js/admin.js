7ebedd76ff3f220f71712556f39ccc565c3dc23b
fardeen Ehsan <fardeen.es7@gmail.com>
2021-06-29 03:34:40 +0600

    Result upload system

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,27 +2,33 @@ var db = firebase.firestore();
 var uid;
 var user = firebase.auth().currentUser;
 
-
 var s = document.getElementById("students");
 var a = document.getElementById("addstudent");
 var c = document.getElementById("construction");
 var pagetitle = document.getElementById("pagetitle")
 var regapprove_modal = document.getElementById("regapprove_modal");
+var sdetails = document.getElementById("sdetails");
+var stinfo = document.getElementById("stinfo");
+var pendingreg = document.getElementById("pendingreg");
+stinfo.style.display = 'none';
+sdetails.style.display="none"
 s.style.display = "block";
 c.style.display = "none";
 a.style.display = "none";
-regapprove_modal.style.display="none";    
+regapprove_modal.style.display="none"; 
 
 function students(){
     s.style.display = "block";
     c.style.display = "none";
     a.style.display = "none";
+    sdetails.style.display="none"
     document.getElementById("pagetitle").innerHTML = "Students";
  
 }
 function addstudent(){
     s.style.display = "none";
     a.style.display = "block";
+    sdetails.style.display="none"
     document.getElementById("pagetitle").innerHTML = "Add Student";
 
 }
@@ -73,6 +79,8 @@ function renderstudent(doc){
     let studentid = document.createElement('span');
 
     li.setAttribute('id', doc.id);
+    li.setAttribute('onclick', 'studentprofile(doc.id);');
+    li.onclick = function() {studentprofile(doc.id);};
     name.textContent = doc.data().Name;
     studentid.textContent = doc.data().studentID;
     
@@ -230,6 +238,7 @@ var sem,c1,c2,c3,c4,c5;
 var foo={};
     
 function regapprove(id){
+
     db.collection('pending').doc(id).get()
         .then(doc=>{
             sem = doc.data().semester;
@@ -240,26 +249,21 @@ function regapprove(id){
             c5 = doc.data().sub_code5;
             if(c1!=''){
                 foo['s1']=c1;
-                foo['r1']=0;
             };
             if(c2!=''){
                 foo['s2']=c2;
-                foo['r2']=0;
             };
             if(c3!=''){
                 foo['s3']=c3;
-                foo['r3']=0;
             };
             if(c4!=''){
                 foo['s4']=c4;
-                foo['r4']=0;
             };
             if(c5!=''){
                 foo['s5']=c5;
-                foo['r5']=0;
             };
             
-            db.collection('student').doc(id).collection('result').doc(sem).set(foo)
+            db.collection('student').doc(id).collection('result').doc(sem).set(foo, { merge: true })
             .catch((error)=>{
                 console.log(error);
                 window.alert(error);
@@ -279,4 +283,154 @@ function deletepending(id){
     }).catch((error) => {
         console.error("Error removing document: ", error);
     });
+}
+function get_gpa(n){
+    gpa=0;
+    grade="";
+    if(n>=80){
+      gpa=4.0;
+      grade = "A+";
+    }else if(n>=75){
+      gpa=3.75;
+      grade="A";
+    }else if(n>=70){
+      gpa=3.50;
+      grade="A-";
+    }else if(n>=65){
+      gpa=3.25;
+      grade="B+";
+    }else if(n>=60){
+      gpa=3.0;
+      grade="B";
+    }else if(n>=55){
+      gpa=2.75;
+      grade="B-";
+    }else if(n>=50){
+      gpa=2.50;
+      grade="C";
+    }else if(n>=45){
+      gpa=2.25;
+      grade="D";
+    }else if(n>=40){
+      gpa=2.0;
+      grade="E";
+    }else if(n<40){
+      gpa=0;
+      grade="F";
+    }
+    return [gpa,grade];
+  }
+function renderresult(doc, sid, semester){
+  update_button = document.getElementById('update_button');
+  update_button.setAttribute('onclick', 'updateresult(sid, semester);');
+  update_button.onclick = function() {updateresult(sid, semester);};
+  var blank = document.getElementsByClassName('blanktable');
+  for (i = 0; i < blank.length; i++) {
+    blank[i].innerHTML = "";
+  }
+    document.getElementById('s1').innerHTML = sub_code(doc.s1);
+    document.getElementById('subn1').innerHTML = eval(doc.s1);
+    document.getElementById('num1').value = doc.r1;
+    let gpa=get_gpa(doc.r1)
+    document.getElementById('gpa1').innerHTML = gpa[0];
+    document.getElementById('grade1').innerHTML = gpa[1];
+    
+    document.getElementById('s2').innerHTML = sub_code(doc.s2);
+    document.getElementById('subn2').innerHTML = eval(doc.s2);
+    document.getElementById('num2').value = doc.r2;
+    gpa=get_gpa(doc.r2)
+    document.getElementById('gpa2').innerHTML = gpa[0];
+    document.getElementById('grade2').innerHTML = gpa[1];
+    
+    document.getElementById('s3').innerHTML = sub_code(doc.s3);
+    document.getElementById('subn3').innerHTML = eval(doc.s3);
+    document.getElementById('num3').value = doc.r3;
+    gpa=get_gpa(doc.r3)
+    document.getElementById('gpa3').innerHTML = gpa[0];
+    document.getElementById('grade3').innerHTML = gpa[1];
+    
+    document.getElementById('s4').innerHTML = sub_code(doc.s4);
+    document.getElementById('subn4').innerHTML = eval(doc.s4);
+    document.getElementById('num4').value = doc.r4;
+    gpa=get_gpa(doc.r4)
+    document.getElementById('gpa4').innerHTML = gpa[0];
+    document.getElementById('grade4').innerHTML = gpa[1];
+    
+    document.getElementById('s5').innerHTML = sub_code(doc.s5);
+    document.getElementById('subn5').innerHTML = eval(doc.s5);
+    document.getElementById('num5').value = doc.r5;
+    gpa=get_gpa(doc.r5)
+    document.getElementById('gpa5').innerHTML = gpa[0];
+    document.getElementById('grade5').innerHTML = gpa[1];    
+  }
+
+
+
+function studentprofile(sid){
+    s.style.display = "none";
+    c.style.display = "none";
+    a.style.display = "none";
+    pendingreg.style.display="none";
+    stinfo.style.display= "block";
+    regapprove_modal.style.display="none"; 
+    sdetails.style.display="block";
+  db.collection('student').doc(sid).get()
+        .then(doc=>{
+        var name = doc.data().Name;
+        var id = doc.data().studentID;
+        document.getElementById("infoname").innerHTML = name;
+        document.getElementById("infoid").innerHTML = id;        
+        })
+    const resultform = document.querySelector('#resultform');
+    resultform.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      const semester = resultform.semesternum.value;
+      document.getElementById('semester').innerHTML = semester+" Semester";
+      db.collection('student').doc(sid).collection('result').doc(semester).get()
+      .then(doc=>{
+        console.log(doc.data());
+      renderresult(doc.data(), sid, semester);
+      }).catch((error) => {
+        console.error("Error: ", error);
+    });
+    resultform.reset();
+    });
+    
+}
+
+function updateresult(sid, semesterv){
+  const resultupdateform = document.querySelector('#resultupdate');
+  resultupdateform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+  const m1 = resultupdate['num1'].value;
+  const m2 = resultupdate['num2'].value;
+  const m3 = resultupdate['num3'].value;
+  const m4 = resultupdate['num4'].value;
+  const m5 = resultupdate['num5'].value;
+  var foo = {};
+  if(m1!=''){
+      foo['r1']=m1;
+  };
+  if(m2!=''){
+      foo['r2']=m2;
+  };
+  if(m3!=''){
+      foo['r3']=m3;
+  };
+  if(m4!=''){
+      foo['r4']=m4;
+  };
+  if(m5!=''){
+      foo['r5']=m5;
+  };
+  db.collection('student').doc(sid).collection('result').doc(semesterv).set(foo, { merge: true })
+  .then(doc=>{
+    window.alert("Result Updated Successfully");
+  })          
+  .catch((error)=>{
+                console.log(error);
+                window.alert(error);
+            })
+  
+})
 }
